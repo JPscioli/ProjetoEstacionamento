6b1cb8a98fa218c72e0655d5d16314e2a9a3c83a
Great Designer Software <45456833+JPscioli@users.noreply.github.com>
2022-04-18 15:10:41 -0300

    Start

---
+++ ProjetoEstacionamento/src/test/java/gds/com/ProjetoEstacionamento/ProjetoEstacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package gds.com.ProjetoEstacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEstacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
